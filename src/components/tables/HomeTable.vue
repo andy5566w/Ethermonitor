9686e2688fcffea260005576e542c78d0c33aa69
andy.tsai <andy.tsai@deephow.com>
2022-07-11 18:26:42 +0800

    remove unnecessary code.

--- src/components/tables/HomeTable.vue
+++ src/components/tables/HomeTable.vue
@@ -132,15 +132,6 @@ const props = defineProps({
   }
   &__footer {
     border-top: 1px solid var(--border-color);
-    &--btn {
-      background-color: rgba(52, 152, 219, 0.1);
-      transition: 0.5s all ease;
-      &:hover {
-        color: #fff;
-        background: #3498db;
-        box-shadow: 0 4px 11px rgb(52 152 219 / 35%);
-      }
-    }
   }
 }
 </style>
