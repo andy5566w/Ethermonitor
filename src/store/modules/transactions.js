3aa6d7ec630019ae4f42d6dc782fad9c5ce5ee7b
andy.tsai <andy.tsai@deephow.com>
2022-08-20 16:56:13 +0800

    adjust vuex(tx)

--- src/store/modules/transactions.js
+++ src/store/modules/transactions.js
@@ -1,12 +1,11 @@
-// import { fetchTxs } from "../../server/ether";
+import { fetchTx } from "../../server/web3";
 
 export const namespaced = true;
 
 export const state = {
-  // Below id is test id
-  transactions: [
-    "0x59f9d2896b5b12e8aac387842076d202f6ae4fdf19951c092570645225287725",
-  ],
+  // Below id is test id： "0x59f9d2896b5b12e8aac387842076d202f6ae4fdf19951c092570645225287725",
+  transactions: [],
+  transactionsIds: [],
 };
 
 export const getters = {
@@ -19,12 +18,20 @@ export const mutations = {
   UPDATE_TRANSACTIONS(state, payload) {
     state.transactions.push(payload);
   },
+  UPDATE_TRANSACTIONS_ID(state, id) {
+    state.transactionsIds.push(id);
+  },
 };
 
 export const actions = {
-  async storeTransactionIds({ commit }, txs = []) {
+  async storeTransactionIds({ commit, dispatch }, txs = []) {
     txs.forEach((id) => {
-      commit("UPDATE_TRANSACTIONS", id);
+      commit("UPDATE_TRANSACTIONS_ID", id);
+      dispatch("fetchTransactionById", id);
     });
+  },
+  async fetchTransactionById({ commit }, txId) {
+    const tx = fetchTx(txId);
+    commit("UPDATE_TRANSACTIONS", tx);
   },
 };
